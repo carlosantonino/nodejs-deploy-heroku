2e34ed8abbde8f09ed05d95978ec263cb5c4e0b1
Carlos Antonino <carlosantonin.cs@gmail.com>
2022-03-17 19:10:58 -0300

    Primeiros passos com handlebars

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const handlebars = requere('express-handlebars');
+const Sequelize = require('sequelize');
+
+
+// Config
+    // Template Engine
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
+    // Conexão com o banco de dados MySql
+    const sequelize = new Sequelize('meubanco','carlos','',{
+        host:"localhost",
+        dialect:"mysql"
+    }); 
+
+app.listen(8081, function(){
+    console.log("Servidor Rodando na url http:localhost:8081");
+});
