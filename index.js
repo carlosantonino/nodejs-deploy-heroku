ea0c6a64612300c328c19faa9c388d54535ffdfb
Carlos <72629502+carlosantonino@users.noreply.github.com>
2022-03-20 08:41:12 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const app = express();
-const handlebars = requere('express-handlebars');
-const Sequelize = require('sequelize');
+
+const port = process.env.PORT || 3000;
+// const handlebars = requere('express-handlebars');
+// const Sequelize = require('sequelize');
 
 
 // Config
     // Template Engine
-        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-        app.set('view engine', 'handlebars')
+        // app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        // app.set('view engine', 'handlebars')
     // Conexão com o banco de dados MySql
-    const sequelize = new Sequelize('meubanco','carlos','',{
-        host:"localhost",
-        dialect:"mysql"
-    }); 
+    // const sequelize = new Sequelize('meubanco','carlos','',{
+    //     host:"localhost",
+    //     dialect:"mysql"
+    // }); 
     
+    app.get("/", function (req, res) {
+        res.send("Olá, Fabrica de Sinopse!");
+    })
 
-app.listen(8081, function(){
-    console.log("Servidor Rodando na url http:localhost:8081");
+app.listen(port, function(){
+    console.info("Servidor Rodando na url http:localhost:3000");
 });
