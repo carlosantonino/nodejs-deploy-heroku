f0a64b4493c02bb9f9ad695ed097b38cd0d8db38
Carlos Antonino <carlosantonin.cs@gmail.com>
2022-03-20 08:23:52 -0300

    para deploy

---
+++ app-mysql-mongo/README.md
@@ -0,0 +1,14 @@
+## Primeiro passo
+## npm init
+
+## Segundo passo
+## npm install express
+
+## Terceiro passo
+
+## Criar arquivo app.js
+
+## Criar Rotas no express
+
+## https://www.youtube.com/watch?v=UMI7kFwmAHo&list=PLJ_KhUnlXUPtbtLwaxxUxHqvcNQndmI4B&index=7
+
---
+++ app-mysql-mongo/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+// const handlebars = requere('express-handlebars');
+// const Sequelize = require('sequelize');
+
+
+// Config
+    // Template Engine
+        // app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        // app.set('view engine', 'handlebars')
+    // Conexão com o banco de dados MySql
+    // const sequelize = new Sequelize('meubanco','carlos','',{
+    //     host:"localhost",
+    //     dialect:"mysql"
+    // }); 
+    
+    app.get("/", function (req, res) {
+        res.send("Olá, Fabrica de Sinopse!");
+    })
+
+app.listen(port, function(){
+    console.info("Servidor Rodando na url http:localhost:3000");
+});
